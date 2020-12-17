eab979ae9864fe4cd706d2d7e5fdc9ea86e3fef0
Drathijin <rsulbara@ucm.es>
2020-12-17 08:05:27 +0100

    ana no me mates creo que va mejor la luz ahora

--- ProyectosOGREvc15x86/media/IG2App/BoyaFS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/BoyaFS.glsl
@@ -7,13 +7,15 @@ uniform sampler2D materialTex1;
 uniform vec3 lightDiffuse;
 uniform vec4 lightPosition;
 
-uniform vec3 materialDiffuse;
-
+//uniform vec3 materialDiffuse;
+uniform vec4 InColor;
+uniform vec4 OutColor; 
 
 in vec2 vUv0;
 in vec3 vVertex;
 in vec3 vNormal;
-in vec3 amb;
+in vec3 amb1;
+in vec3 amb2;
 
 
 out vec4 fFragColor;
@@ -33,11 +35,11 @@ float diff(vec3 cVertex, vec3 cNormal)
 void main()
 {
 
-	vec3 diffuse = diff(vVertex, vNormal) * lightDiffuse * materialDiffuse;
-	vec3 inColor = amb + diffuse;
+	vec3 diffuse = diff(vVertex, vNormal) * lightDiffuse;
+	vec3 inColor = amb1 + diffuse;
 	
-	diffuse = diff(vVertex, -vNormal) * lightDiffuse * materialDiffuse;
-	vec3 outColor = amb + diffuse;
+	diffuse = diff(vVertex, -vNormal) * lightDiffuse;
+	vec3 outColor = amb2 + diffuse;
 
 
 	vec3 m1  = vec3(texture(materialTex0, vUv0));
@@ -47,9 +49,9 @@ void main()
 	vec3 m2  = vec3(texture(materialTex1, vUv0));
 	
 	if(gl_FrontFacing)
-		m1 = vec3(inColor);
+		m1 = vec3(outColor);
 	else 
-		m2 = vec3(outColor);
+		m1 = vec3(inColor);
 	
-	fFragColor = vec4(m1 * m2,1);
+	fFragColor = vec4(m1*m2,1);
 }
--- ProyectosOGREvc15x86/media/IG2App/BoyaVS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/BoyaVS.glsl
@@ -5,9 +5,11 @@ uniform mat4 modelViewProjMat;
 uniform mat4 modelViewMat;
 uniform mat4 normalMat;
 
+
 uniform vec3 lightAmbient;
 
-uniform vec3 materialDiffuse;
+uniform vec4 InColor;
+uniform vec4 OutColor; 
 
 in vec4 vertex;
 in vec3 normal;
@@ -17,12 +19,14 @@ in vec2 uv0;
 out vec2 vUv0;
 out vec3 vVertex;
 out vec3 vNormal;
-out vec3 amb;
+out vec3 amb1;
+out vec3 amb2;
 
 
 void main()
 {
-	amb = lightAmbient * materialDiffuse;
+	amb1 = lightAmbient * vec3(InColor);
+	amb2 = lightAmbient * vec3(OutColor);
 
 	vec3 viewVertex = vec3(modelViewMat * vertex);
 	vec3 viewNormal = normalize(vec3(normalMat * vec4(normal, 0)));
